6b6fb78c945cb57471874bc4da9be5389759fc1c
Muhammed Ijlan <ijlanijlu580@gmail.com>
2026-06-10 11:12:35 +0400

    feat(admin): register chart.js components

---
+++ components/admin/charts/registry.ts
@@ -0,0 +1,24 @@
+import {
+  Chart,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  BarElement,
+  ArcElement,
+  Filler,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+Chart.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  BarElement,
+  ArcElement,
+  Filler,
+  Tooltip,
+  Legend
+);
